0ac8faff1bdc923d4ab01cd232c8421075f92183
gonexwind <fikky.ardi15@gmail.com>
2021-04-13 09:02:44 +0700

    add data

--- lib/data.dart
+++ lib/data.dart
@@ -1,6 +1,5 @@
 var data = [
   {
-    "id": 0,
     "title": "Niat Puasa Ramadhan",
     "doa":
         "نَوَيْتُ صَوْمَ غَدٍ عَنْ أَدَاءِ فَرْضِ شَهْرِ رَمَضَانَ هذِهِ السَّنَةِ لِلهِ تَعَالَى",
@@ -10,7 +9,6 @@ var data = [
         "Aku berniat puasa esok hari demi menunaikan kewajiban bulan Ramadhan tahun ini karena Allah ta’ala.",
   },
   {
-    "id": 1,
     "title": "Doa Berbuka Puasa",
     "doa":
         "اللَّهُمَّ لَكَ صُمْتُ وَعَلى رِزْقِكَ أفْطَرْتُ ذَهَبَ الظَّمأُ وابْتَلَّتِ العُرُوقُ وَثَبَتَ الأجْرُ إِنْ شاءَ اللَّهُ تَعالى",
@@ -20,32 +18,82 @@ var data = [
         "Ya Alloh, untuk-Mulah aku berpuasa, atas rezekimulah aku berbuka. Telah sirna rasa dahaga, urat-urat telah basah, dan (semoga) pahala telah ditetapkan, insya alloh.",
   },
   {
-    "id": 1,
-    "title": "",
+    "title": "Niat Sholat Tarawih",
+    "doa":
+        "اُصَلِّى سُنَّةَ التَّرَاوِيْحِ رَكْعَتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً إِمَامًا ِللهِ تَعَالَى",
+    "latin":
+        "Usholli sunnatat Tarāwīhi rak‘atayni mustaqbilal qiblati adā’an imāman lillāhi ta‘ālā.",
+    "terjemahan":
+        "Aku menyengaja sembahyang sunnah Tarawih dua rakaat dengan menghadap kiblat, tunai sebagai imam karena Allah SWT.",
+  },
+  {
+    "title": "Bilal Tarawih & Jawabannya",
     "doa": "",
     "latin": "",
     "terjemahan": "",
   },
   {
-    "id": 1,
-    "title": "",
+    "title": "Doa Kamilin Bakda Tarawih",
+    "doa":
+        "اَللهُمَّ اجْعَلْنَا بِالْإِيْمَانِ كَامِلِيْنَ. وَلِلْفَرَائِضِ مُؤَدِّيْنَ. وَلِلصَّلاَةِ حَافِظِيْنَ. وَلِلزَّكَاةِ فَاعِلِيْنَ. وَلِمَا عِنْدَكَ طَالِبِيْنَ. وَلِعَفْوِكَ رَاجِيْنَ. وَبِالْهُدَى مُتَمَسِّكِيْنَ. وَعَنِ الَّلغْوِ مُعْرِضِيْنَ. وَفِى الدُّنْيَا زَاهِدِيْنَ. وَفِى اْلآخِرَةِ رَاغِبِيْنَ. وَبَالْقَضَاءِ رَاضِيْنَ. وَلِلنَّعْمَاءِ شَاكِرِيْنَ. وَعَلَى الْبَلاَءِ صَابِرِيْنَ. وَتَحْتَ لِوَاءِ مُحَمَّدٍ صَلَّى اللهُ عَلَيْهِ وَسَلَّمَ يَوْمَ الْقِيَامَةِ سَائِرِيْنَ وَعَلَى الْحَوْضِ وَارِدِيْنَ. وَإِلَى الْجَنَّةِ دَاخِلِيْنَ. وَمِنَ النَّارِ نَاجِيْنَ. وَعَلى سَرِيْرِالْكَرَامَةِ قَاعِدِيْنَ. وَبِحُوْرٍعِيْنٍ مُتَزَوِّجِيْنَ. وَمِنْ سُنْدُسٍ وَاِسْتَبْرَقٍ وَدِيْبَاجٍ مُتَلَبِّسِيْنَ. وَمِنْ طَعَامِ الْجَنَّةِ آكِلِيْنَ. وَمِنْ لَبَنٍ وَعَسَلٍ مُصَفًّى شَارِبِيْنَ. بِأَكْوَابٍ وَّأَبَارِيْقَ وَكَأْسٍ مِّنْ مَعِيْن. مَعَ الَّذِيْنَ أَنْعَمْتَ عَلَيْهِمْ مِنَ النَّبِيِّيْنَ وَالصِّدِّيْقِيْنَ وَالشُّهَدَآءِ وَالصَّالِحِيْنَ وَحَسُنَ أُولئِكَ رَفِيْقًا. ذلِكَ الْفَضْلُ مِنَ اللهِ وَكَفَى بِاللهِ عَلِيْمًا. اَللهُمَّ اجْعَلْنَا فِى هذِهِ اللَّيْلَةِ الشَّهْرِالشَّرِيْفَةِ الْمُبَارَكَةِ مِنَ السُّعَدَاءِ الْمَقْبُوْلِيْنَ. وَلاَتَجْعَلْنَا مِنَ اْلأَشْقِيَاءِ الْمَرْدُوْدِيْنَ. وَصَلَّى اللهُ عَلَى سَيِّدِنَا مُحَمَّدٍ وَآلِه وَصَحْبِه أَجْمَعِيْنَ. بِرَحْمَتِكَ يَاأَرْحَمَ الرَّاحِمِيْنَ وَالْحَمْدُ لِلّهِ رَبِّ الْعَالَمِيْنَ",
+    "latin":
+        "Allâhummaj‘alnâ bil îmâni kâmilîn. Wa lil farâidli muaddîn. Wa lish-shlâti hâfidhîn. Wa liz-zakâti fâ‘ilîn. Wa lima ‘indaka thâlibîn. Wa li ‘afwika râjîn. Wa bil-hudâ mutamassikîn. Wa ‘anil laghwi mu‘ridlîn. Wa fid-dunyâ zâhdîn. Wa fil ‘âkhirati râghibîn. Wa bil-qadlâ’I râdlîn. Wa lin na‘mâ’I syâkirîn. Wa ‘alal balâ’i shâbirîn. Wa tahta liwâ’i muhammadin shallallâhu ‘alaihi wasallam yaumal qiyâmati sâ’irîna wa alal haudli wâridîn. Wa ilal jannati dâkhilîn. Wa minan nâri nâjîn. Wa 'alâ sariirl karâmati qâ'idîn. Wa bi hûrun 'in mutazawwijîn. Wa min sundusin wa istabraqîn wadîbâjin mutalabbisîn. Wa min tha‘âmil jannati âkilîn. Wa min labanin wa ‘asalin mushaffan syâribîn. Bi akwâbin wa abârîqa wa ka‘sin min ma‘în. Ma‘al ladzîna an‘amta ‘alaihim minan nabiyyîna wash shiddîqîna wasy syuhadâ’i wash shâlihîna wa hasuna ulâ’ika rafîqan. Dâlikal fadl-lu minallâhi wa kafâ billâhi ‘alîman. Allâhummaj‘alnâ fî hâdzihil lailatisy syahrisy syarîfail mubârakah minas su‘adâ’il maqbûlîn. Wa lâ taj‘alnâ minal asyqiyâ’il mardûdîn. Wa shallallâhu ‘alâ sayyidinâ muhammadin wa âlihi wa shahbihi ajma‘în. Birahmatika yâ arhamar râhimîn wal hamdulillâhi rabbil ‘âlamîn.",
+    "terjemahan":
+        "Yaa Allah, jadikanlah kami orang-orang yang sempurna imannya, yang memenuhi kewajiban-kewajiban, yang memelihara shalat, yang mengeluarkan zakat, yang mencari apa yang ada di sisi-Mu, yang mengharapkan ampunan-Mu, yang berpegang pada petunjuk, yang berpaling dari kebatilan, yang zuhud di dunia, yang menyenangi akhirat, yang ridha dengan qadla-Mu (ketentuan-Mu), yang mensyukuri nikmat, yang sabar atas segala musibah, yang berada di bawah panji-panji junjungan kami, Nabi Muhammad, pada hari kiamat, yang mengunjungi telaga (Nabi Muhammad), yang masuk ke dalam surga, yang selamat dari api neraka, yang duduk di atas ranjang kemuliaan, yang menikah dengan para bidadari, yang mengenakan berbagai sutra ,yang makan makanan surga, yang minum susu dan madu murni dengan gelas, cangkir, dan cawan bersama orang-orang yang Engkau beri nikmat dari kalangan para nabi, shiddiqin, syuhada dan orang-orang shalih. Mereka itulah teman yang terbaik. Itulah keutamaan (anugerah) dari Allah, dan cukuplah bahwa Allah Maha Mengetahui. Ya Allah, jadikanlah kami pada malam yang mulia dan diberkahi ini termasuk orang-orang yang bahagia dan diterima amalnya, dan janganlah Engkau jadikan kami tergolong orang-orang yang celaka dan ditolak amalnya. Semoga Allah mencurahkan rahmat-Nya atas junjungan kami Muhammad, serta seluruh keluarga dan shahabat beliau. Berkat rahmat-Mu, wahai Yang Paling Penyayang di antara yang penyayang. Segala puji bagi Allah Tuhan semesta alam.",
+  },
+  {
+    "title": "Niat Sholat Witir",
+    "doa":
+        "اُصَلِّى سُنَّةَ مِنَ الوِتْرِ رَكْعَتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً مَأْمُوْمًا لِلهِ تَعَالَى",
+    "latin":
+        "Usholli sunnatan minal Witri rak‘ataini mustaqbilal qiblati adā’an makmūman lillāhi ta‘ālā.",
+    "terjemahan":
+        "Aku menyengaja sembahyang sunnah bagian dari shalat Witir dua rakaat dengan menghadap kiblat, tunai sebagai makmum karena Allah SWT.",
+  },
+  {
+    "title": "Wirid & Doa Bakda Witir",
     "doa": "",
     "latin": "",
     "terjemahan": "",
   },
-];
-
-List menu = [
-  'Niat Puasa Ramadhan',
-  'Doa Usa Buka Puasa',
-  'Niat Sholat Tarawih',
-  'Bilal Tarawih & Jawabannya',
-  'Doa Kamilin Bakda Tarawih',
-  'Niat Sholat Witir',
-  'Wirid & Doa Bakda Witir',
-  'Bacaan Lailatur Qodar',
-  'Niat Zakat Fitrah',
-  'Doa Menerima Zakat Fitrah',
-  'Lafazd Takbiran Idul Fitri',
-  'Niat Sholat Idul Fitri',
+  {
+    "title": "Bacaan Lailatur Qodar",
+    "doa": "اللهم انك عفو تحب العفو فاعف عني",
+    "latin": "Allohumma innaka ‘afuwwun tuhibbul ‘afwa fa’fu ‘anni’",
+    "terjemahan":
+        "Wahai Tuhan, Engkau Maha Pengampun, menyukai orang yang minta ampunan, ampunilah aku.",
+  },
+  {
+    "title": "Niat Zakat Fitrah",
+    "doa":
+        "ﻧَﻮَﻳْﺖُ أَﻥْ أُﺧْﺮِﺝَ ﺯَﻛَﺎﺓَ ﺍﻟْﻔِﻄْﺮِ ﻋَﻦْ ﻧَﻔْسيْ ﻓَﺮْﺿًﺎ ِﻟﻠﻪِ ﺗَﻌَﺎﻟَﻰ",
+    "latin":
+        "Nawaitu an ukhrija zakatalfithri 'an nafsi fardholiullahi ta 'aala",
+    "terjemahan":
+        "Aku niat mengeluarkan zakat fitrah untuk diriku sendiri, fardu karena Allah Ta‘âlâ.",
+  },
+  {
+    "title": "Doa Menerima Zakat Fitrah",
+    "doa":
+        "طَهَّرَ اللهُ قَلْبَكَ فِي قُلُوْبِ الأَبْرَارِ وَزَكَّى عَمَلَكَ فِي عَمَلِ الأَخْيَارِ وَصَلَّى عَلَى رُوْحِكَ فِي أَرْوَاحِ الشُّهَدَاءِ",
+    "latin":
+        "Thohharallāhu qalbaka fī qulūbil abrār, wa zakkā ‘amalaka fī ‘amalil akhyār, wa shallā ‘alā rūhika fī arwāhis syuhadā’.",
+    "terjemahan":
+        "Semoga Allah menyucikan hatimu pada hati para hamba-Nya yang abrar. Semoga Allah bersihkan amalmu pada amal para hamba-Nya yang akhyar. Semoga Allah bershalawat untuk rohmu pada roh para hamba-Nya yang syahid.",
+  },
+  {
+    "title": "Lafazd Takbiran Idul Fitri",
+    "doa": "",
+    "latin": "",
+    "terjemahan": "",
+  },
+  {
+    "title": "Niat Sholat Idul Fitri",
+    "doa":
+        "اُصَلِّى سُنَّةً لِعِيْدِ الفِطْرِ رَكْعَتَيْنِ مُسْتَقْبِلَ الْقِبْلَةِ أَدَاءً مَأْمُوْمًا لِلهِ تَعَالَى",
+    "latin":
+        "Usholli sunnatan li Idil Fitri rak‘atayni mustaqbilal qiblati adā’an ma’mūman lillāhi ta‘ālā.",
+    "terjemahan":
+        "Aku menyengaja sembahyang sunnah Idul Fitri dua rakaat dengan menghadap kiblat, tunai sebagai makmum karena Allah SWT.",
+  },
 ];
